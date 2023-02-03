fca9412b811cb33d768bd381979610ca6a3ad833
Quinty <quintydewinter1@gmail.com>
2023-02-03 12:37:03 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Maak je eigen pizza</h2>
+
+<form>
+  <label>Bodemformaat:</label>
+  <select>
+    <option value="20">20</option>
+    <option value="25">25</option>
+    <option value="30">30</option>
+    <option value="35">35</option>
+    <option value="40">40</option>
+  </select>
+
+<br>
+<br>
+
+  <label>Saus:</label>
+  <select>
+    <option value="tomatensaus">Tomatensaus</option>
+    <option value="extra tomatensaus">Extra Tomatensaus</option>
+    <option value="spicy pimentosaus">Spicy Pimentosaus</option>
+    <option value="bbq Saus">BBQ Saus</option>
+    <option value="creme fraiche">Creme fraiche</option>
+  </select>
+
+<br>
+<br>
+
+  <label>Pizzatoppings:</label>
+  <br>
+  <input type="radio" id="vegan" name="" value="">
+        <label for="html">Vegan</label><br>
+        <input type="radio" id="vegatarsich" name="" value="">
+        <label for="css">Vegatarisch</label><br>
+        <input type="radio" id="vlees" name="" value="">
+        <label for="javascript">Vlees</label>
+<br>
+<br>
+ 
+<label> Kruiden: </label>
+<br>
+<input type="checkbox" id="" name="" value="">
+        <label for="">Peterselie</label><br>
+        <input type="checkbox" id="" name="" value="">
+        <label for="">Oregano</label><br>
+        <input type="checkbox" id="" name="" value="">
+        <label for="">Chili Flakes</label> <br>
+        <input type="checkbox" name= "" value="">
+        <label for="">Zwarte peper</label>
+</form>
+
+
+</body>
+</html>
